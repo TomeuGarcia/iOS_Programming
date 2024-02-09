e5d4dd261f8b57a188e63074923e1d130383ad3c
Tomeu Garcia Pinto <tomeu.garcia@enti.cat>
2024-02-09 20:29:46 +0100

    Update project

---
+++ iOS_Project/TestBindingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TestBindingViewController.swift
+//  iOS_Project
+//
+//  Created by Tomeu Garcia Pinto on 9/2/24.
+//
+
+import UIKit
+
+class TestBindingViewController: UIViewController 
+{
+
+    @IBOutlet weak var _boxRed: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func HelloWorld(_ sender: Any, forEvent event: UIEvent) {
+    }
+    
+}
