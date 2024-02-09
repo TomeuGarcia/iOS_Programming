abacb1a30b60eca4172bf493df0cd6d6529c3518
Tomeu Garcia Pinto <tomeu.garcia@enti.cat>
2024-02-09 19:11:00 +0100

    Initial commit

---
+++ iOS_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Project
+//
+//  Created by Tomeu Garcia Pinto on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
