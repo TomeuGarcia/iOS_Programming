b4e062ec9539691f94f53f167c1c9320c2d2d195
Tomeu Garcia Pinto <tomeu.garcia@enti.cat>
2024-05-03 16:44:52 +0200

    Add Alerts test

--- iOS_Project/Views/VCs/StartVC.swift
+++ iOS_Project/Views/VCs/StartVC.swift
@@ -31,7 +31,50 @@ class StartVC : VC {
         self.view.backgroundColor = .gray
 
 
+        testAlerts()
     }
     
+    func testAlerts() {
+        
+        self.countButton.onClick = {
+            let alert = UIAlertController(
+                title: "My first Pop Up",
+                message: "Long Long Long Message\n Second Line",
+                preferredStyle: .alert)
+            
+            var tempTextField: UITextField? = nil
+            
+            alert.addTextField { textField in
+                tempTextField = textField
+                textField.placeholder = "Placeholder text"
+            }
+            
+            let okButton = UIAlertAction(
+                title: "Ok",
+                style: .default) { action in
+                    self.countButton.label?.text = tempTextField?.text
+                }
+            
+            let cancelButton = UIAlertAction(
+                title: "Cancel",
+                style: .cancel)
+
+            let destroyButton = UIAlertAction(
+                title: "Destroy",
+                style: .destructive) { action in
+                self.countButton.label?.text = "Destroyed"
+                self.countButton.label?.textColor = .red
+            }
+            
+
+            
+            alert.addAction(okButton)
+            alert.addAction(cancelButton)
+            alert.addAction(destroyButton)
+            
+            self.present(alert, animated: true)
+        }
+        
+    }
     
 }
